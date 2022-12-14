68280fe81f5cb56a318095cf41d02d220dd435da
Marco Eidinger <eidingermarco@gmail.com>
2022-12-14 09:56:27 -0800

    SwiftPM

---
+++ Sources/swift-api-assign-reviewer-test/swift_api_assign_reviewer_test.swift
@@ -0,0 +1,6 @@
+public struct swift_api_assign_reviewer_test {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/swift-api-assign-reviewer-testTests/swift_api_assign_reviewer_testTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import swift_api_assign_reviewer_test
+
+final class swift_api_assign_reviewer_testTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swift_api_assign_reviewer_test().text, "Hello, World!")
+    }
+}
