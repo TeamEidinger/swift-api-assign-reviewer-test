c500728d7c3beeff5b7d964d98cb61735305bc63
Marco Eidinger <eidingermarco@gmail.com>
2022-12-14 10:14:13 -0800

    requires

--- Sources/swift-api-assign-reviewer-test/swift_api_assign_reviewer_test.swift
+++ Sources/swift-api-assign-reviewer-test/swift_api_assign_reviewer_test.swift
@@ -1,9 +1,7 @@
 public struct swift_api_assign_reviewer_test {
     public private(set) var text = "Hello, World!"
 
-    public init() {
-        // dummy change
-    }
+    public init() {}
 
     public func test() {
         // requires reviewer
